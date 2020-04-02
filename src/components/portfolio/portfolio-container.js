1e8550199c9e776de94388b6d6c96b779be66ab5
dallinhumphrey <dallinhumphrey@gmail.com>
2020-04-02 13:03:33 -0600

    additions

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -6,22 +6,28 @@ export default class PortfolioContainer extends Component {
     constructor() {
         super();
 
-        console.log("Portfolio container has rendered")
+        this.state = {
+            pageTitle: "Welcome to my portfolio",
+            data: [
+                { title: "Good Earth" },
+                { title: "Insidesales" },
+                { title: "NetDocuments" },
+                { title: "Pineapple Promises" }
+            ]
+        }
     }
 
 
     portfolioItems() {
-        const data = ["Good Earth", "Insidesales", "NetDocuments"];
-
-        return data.map(item => {
-            return <PortfolioItem title={item} url={"google.com"} />
+        return this.state.data.map(item => {
+            return <PortfolioItem title={item.title} url={"google.com"} />
         })
     }
 
     render() {
         return (
             <div>
-                <h2>Portfolio Item</h2>
+                <h2>{this.state.pageTitle}</h2>
 
                 {this.portfolioItems()}
             </div>
