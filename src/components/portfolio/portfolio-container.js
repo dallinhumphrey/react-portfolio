21324ceaeb102bb36642d6b5d18f496ad35056cd
dallinhumphrey <dallinhumphrey@gmail.com>
2020-04-08 13:27:52 -0600

    adding buttons

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -8,15 +8,25 @@ export default class PortfolioContainer extends Component {
 
         this.state = {
             pageTitle: "Welcome to my portfolio",
+            isLoading: false,
             data: [
-                { title: "Good Earth" },
-                { title: "Insidesales" },
-                { title: "NetDocuments" },
-                { title: "Pineapple Promises" }
+                { title: "Good Earth", category: "Management" },
+                { title: "Insidesales", category: "Sales" },
+                { title: "NetDocuments", category: "Sales" },
+                { title: "Pineapple Promises", category: "Product Development" }
             ]
         }
+
+        this.handlePageTitleUpdate = this.handleFilter.bind(this)
     }
 
+    handleFilter(filter) {
+        this.setState({
+            data: this.state.data.filter(item => {
+                return item.category === filter
+            })
+        })
+    }
 
     portfolioItems() {
         return this.state.data.map(item => {
@@ -24,10 +34,28 @@ export default class PortfolioContainer extends Component {
         })
     }
 
+    handlePageTitleUpdate() {
+        this.setState({
+            pageTitle: "Something Else"
+        })
+    }
     render() {
+        if (this.state.isLoading) {
+            return <div>Loading...</div>
+        }
         return (
             <div>
                 <h2>{this.state.pageTitle}</h2>
+
+                <button onClick={() => this.handleFilter("Management")}>
+                    Management
+            </button>
+                <button onClick={() => this.handleFilter("Sales")}>
+                    Sales
+            </button>
+                <button onClick={() => this.handleFilter("Product Development")}>
+                    Product Development
+            </button>
 
                 {this.portfolioItems()}
             </div>
