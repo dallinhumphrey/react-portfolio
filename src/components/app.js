f420bb95acc78b8c1c8d4c5dc86ff71e4bcd3c59
dallinhumphrey <dallinhumphrey@gmail.com>
2020-04-15 15:39:18 -0600

    hidden links

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import axios from "axios";
 
 import NavigationContainer from "./navigation/navigation-container";
 import Home from "./pages/home";
@@ -34,12 +35,48 @@ export default class App extends Component {
     });
   }
 
+  checkLoginStatus() {
+    return axios.get("https://api.devcamp.space/logged_in", {
+      withCredentials: true
+    })
+      .then(response => {
+        const loggedIn = response.data.logged_in
+        const loggedInStatus = this.state.loggedInStatus
+
+        if (loggedIn && loggedInStatus === "LOGGED_IN") {
+          return loggedIn
+        } else if (loggedIn && loggedInStatus === "NOT_LOGGED_IN") {
+          this.setState({
+            loggedInStatus: "LOGGED_IN"
+          })
+        } else if (!loggedIn && loggedInStatus === "NOT_LOGGED_IN") {
+          this.setState({
+            loggedInStatus: "NOT_LOGGED_IN"
+          })
+        }
+      })
+      .catch(error => {
+        console.log("Error", error)
+      })
+  }
+
+  componentDidMount() {
+    this.checkLoginStatus()
+  }
+
+  authorizedPages() {
+    return [
+      <Route path="/blog" component={Blog} />
+
+    ]
+  }
+
   render() {
     return (
       <div className="container">
         <Router>
           <div>
-            <NavigationContainer />
+            <NavigationContainer loggedInStatus={this.state.loggedInStatus} />
 
             <h2>{this.state.loggedInStatus}</h2>
 
@@ -59,7 +96,9 @@ export default class App extends Component {
 
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
-              <Route path="/blog" component={Blog} />
+              {this.state.loggedInStatus === "LOGGED_IN" ? (
+                this.authorizedPages()
+              ) : null}
               <Route
                 exact
                 path="/portfolio/:slug"
