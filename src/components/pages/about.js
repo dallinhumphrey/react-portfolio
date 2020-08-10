b8c58e9eb74ec24ac04e5cfb6fb26a957fbb3b7e
dallinhumphrey <dallinhumphrey@gmail.com>
2020-08-10 11:53:54 -0600

    fixing about me

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -13,17 +13,9 @@ export default function () {
         }}
       />
       <div className="right-column">
-        This isn't Game of Thrones, Morty. Pluto's a planet. How do you saddle a
-        fart? You want to see my first boner, or should we go straight to the
-        moment I discovered interdimensional travel? Then it's time to get your
-        beak wet tonight playa! What, cover me in gasoline and spiders? Fine,
-        yeah, I'm in. You created a day care for my dad? Lick, lick, lick my
-        balls! Did you just come into the cafeteria through a portal? This is
-        Principal Vagina. No relation. Full disclosure, Morty - it's not.
-        Temporary superintelligence is just a side effect of the Megaseeds
-        dissolving in your rectal cavity. I'd like to order one large person
-        with extra people please. white people, no no no black people... and
-        hispanic on half.
+        My Interests ● Exploring, adventuring, and people. ● Building Lego’s and
+        Warhammer figures with my son. ● Cooking with my wife. ● Listening to
+        music and dancing with my daughter. ● Landscaping and gardening
       </div>
     </div>
   );
